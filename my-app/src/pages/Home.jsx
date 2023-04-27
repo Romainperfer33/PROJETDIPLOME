b3bb973bd71c245f9cb461005975e6afdc3f83bb
Romain Pereira Ferreira <romain.pereira@lapiscine.pro>
2023-04-27 11:28:56 +0200

    commit du jour

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -9,10 +9,13 @@ const Home = () => {
                 <h3>NOTRE CONCEPT</h3>
                     <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum odit unde odio dolores explicabo veniam earum exercitationem, maxime expedita, qui dolorem modi delectus rerum dignissimos omnis. Quae maiores delectus optio at doloremque sequi sed dolor dolorem, fugiat perspiciatis dolorum perferendis ipsum a odit obcaecati cumque harum sunt fugit blanditiis commodi esse molestias impedit. Possimus quaerat qui iure, reiciendis incidunt eligendi molestias non. Facilis, voluptas. Officia fuga maiores modi rem cum quae ab, hic perferendis ex officiis praesentium dolor dicta illum saepe. Cumque eos sapiente provident nulla, expedita dolorum optio ad sed amet nihil, quia numquam vitae obcaecati dolor veritatis quisquam.
                     </p>
-                    <a href='/' className='btn'> EN SAVOIR PLUS</a>
+                <a href='/' className='btn'> EN SAVOIR PLUS</a>
+            </div>
+            <div className='divtrans'>
+
             </div>
                 
-            </div>
+        </div>
     )
 }
 
