9b834000b6183decee2afb7f1f2eab6668c1a73e
Romain Pereira Ferreira <romain.pereira@lapiscine.pro>
2023-04-28 21:09:22 +0200

    stage page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import Home from './pages/Home';
+import Stages from './pages/Stages'
 import './App.css'
 
 function App() {
   return (
     <div id="test">
-      <Home />
+      {/* <Home /> */}
+      <Stages />
     </div>
   );
 }
---
+++ my-app/src/pages/Stages.jsx
@@ -0,0 +1,36 @@
+import './stages.css'
+
+const Stages = () => {
+
+    return (
+        <div className='stagepage'>
+            <div className='stagenavtitle'>
+                <p className='stagepagetitle'>TFA</p>
+                <p className='stagepagetitle'>STAGES</p>
+            </div>
+        
+            <div className='stageintroduce'>
+                <div className='stagepresentation'>
+                    <div className='blueline'>
+                        <p className='stagetitle'>STAGE 1</p>
+                    </div>
+                </div>
+
+                <div className='stagepresentation'>
+                    <div className='blueline'>
+                        <p className='stagetitle'>STAGE 2</p>
+                    </div>
+                </div>
+
+                <div className='stagepresentation'>
+                    <div className='blueline'>
+                        <p className='stagetitle'>STAGE 3</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+
+}
+export default Stages 
